1da3a507c76e594f095458337cf99ce15cbbd5c8
cmehrabian <cameron.mehrabian@gmail.com>
2017-08-04 15:52:25 -0700

    centers map on users location onLoad

--- client/src/components/map/map.jsx
+++ client/src/components/map/map.jsx
@@ -5,13 +5,34 @@ import { TOKEN } from './config.js';
 export class MapContainer extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      currentLocation: {
+        latitude: 35.8617,
+        longitude: 104.1954
+      }
+    }
+  }
+  componentDidMount() {
+    if( navigator && navigator.geolocation ) {
+      navigator.geolocation.getCurrentPosition((pos) => {
+        const { latitude, longitude } = pos.coords;
+        this.setState({
+          currentLocation: { latitude, longitude }
+        })
+      });
+    }
   }
   render() {
+    const style = {
+      width: '100vw',
+      height: '100vh'
+    }
     if (!this.props.loaded) {
       return <div>Loading...</div>
     }
     return (
       <Map
+        style={style}
         google={this.props.google}
         zoom={14}>
       </Map>
